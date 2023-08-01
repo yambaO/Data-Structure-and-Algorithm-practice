cbee0dd4aeab59221bad4099d859a08136edf3ef
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-08-01 18:48:07 -0500

    Adding array search

---
+++ src/Arrays/ArraySearch.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class ArraySearch {
+    public static int numberOfCompares = -1;
+    public static int numberOfSwaps = -1;
+
+    public static <E extends Comparable<E>> int binarySearch(E[] array, E number) {
+        numberOfCompares = 0;
+        numberOfSwaps = 0;
+
+        int lowerBound = 0;
+        int upperBound = array.length;
+
+        while (lowerBound < upperBound) {
+            final int currentIndex = lowerBound + upperBound / 2;
+
+            numberOfCompares++;
+
+            if (number.compareTo(array[currentIndex]) == 0) {
+                return currentIndex;
+            } else if (number.compareTo(array[currentIndex]) < 0) {
+                lowerBound = currentIndex + 1;
+            } else {
+                upperBound = currentIndex - 1;
+            }
+        }
+        return -1;
+    }
+
+    private ArraySearch() {
+    }
+
+}
